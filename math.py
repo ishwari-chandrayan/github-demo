c7f2468d229c8133bef6f77ae1398ee344859753
ishwari-chandrayan <97955351+ishwari-chandrayan@users.noreply.github.com>
2022-01-27 12:51:39 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+# add implementation
+def add(x,y):
+    return x+y  # On Bug123
+
+# subtract implementation
+def subtract(x,y):
+    pass
+
+# multiply implementation
+def multiply(x,y):
+    pass
+
+# divide implementation
+def divide(x,y):
+    pass
